35d136a8340188e581f1dd6f5a4376699ff24ea3
md rayhan munshi <jaber.ac1983@gmail.com>
2025-01-02 01:21:38 +0600

    find product name and added to list

--- web scrap.py
+++ web scrap.py
@@ -2,10 +2,31 @@ import requests as rq
 
 from bs4 import BeautifulSoup as bs
 
-my_url = 'https://www.othoba.com/smartphone'
+my_url = 'https://bigshopbd.com/product-category/home-and-living'
 
 p_html = rq.get(my_url)
 
 p_soup = bs(p_html.text,'html.parser')
-print(p_soup)
 
+name_con = p_soup.findAll('div',{'class':'product-content'})
+price_con = p_soup.findAll('h6',{'class':'product-price'})
+
+
+filename = 'smartphone.csv'
+
+f = open(filename, 'w')
+
+headers = 'Phone Name, Price\n'
+
+f.write(headers)
+
+p_name = []
+p_price = []
+
+for i in name_con:
+    p_name.append(i.h6.a.text)
+
+for i in price_con:
+    p_price.append(i.span.b.text)
+
+print(p_price)
